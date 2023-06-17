751861b0e29097660883dd89aa98636a20e24022
m9 <paulbravomacedo@gmail.com>
2023-06-17 17:46:01 -0500

    Lamba update on click

--- app/src/main/java/uch/quality/AppCalidadSoftware/MainActivity.java
+++ app/src/main/java/uch/quality/AppCalidadSoftware/MainActivity.java
@@ -2,15 +2,9 @@ package uch.quality.AppCalidadSoftware;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,14 +22,11 @@ public class MainActivity extends AppCompatActivity {
         password = findViewById(R.id.password);
         loginButton = findViewById(R.id.loginButton);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
-                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
-                }
+        loginButton.setOnClickListener(view -> {
+            if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
             }
         });
     }
